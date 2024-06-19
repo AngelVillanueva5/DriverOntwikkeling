1b4141b937e21c994650907669d91e065593a2db
AngelVillanueva5 <sd.villanueva@student.avans.nl>
2024-06-19 09:20:28 +0200

    afdsa

--- weekFinal/drvoEindopdracht.c
+++ weekFinal/drvoEindopdracht.c
@@ -12,14 +12,15 @@
 #include <linux/ioport.h>
 #include <linux/i2c.h>
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Johannes 4 GNU/Linux");
 MODULE_DESCRIPTION("A driver for my simple AMTEGA I2C ADC");
-
 static struct i2c_client *i2c_client;
 static struct class *i2c_custom_class;
 static struct device *i2c_custom_device;
-
-static int i2c_custom_remove(struct i2c_client *client);
-static int i2c_custom_probe(struct i2c_client *client, const struct i2c_device_id *id);
+static struct platform_device *adc_pdev;
+static int i2c_custom_remove(struct platform_device *pdev);
+static int i2c_custom_probe(struct platform_device *pdev);
 static ssize_t my_show(struct device *dev, struct device_attribute *attr, char *buf);
 static ssize_t my_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count);
 
@@ -28,23 +29,14 @@ static const struct of_device_id my_driver_ids[] = {
     { }
 };
 
-static const struct i2c_device_id i2c_ids[] = {
-    { "drvoi2c", 0 },
-    { }
-};
-
-struct i2c_driver my_driver = {
+static struct platform_driver my_driver = {
     .probe = i2c_custom_probe,
     .remove = i2c_custom_remove,
     .driver = {
         .name = "drvoi2c",
-        .owner = THIS_MODULE,
         .of_match_table = of_match_ptr(my_driver_ids),
     },
-    .id_table = i2c_ids,
 };
-
-MODULE_DEVICE_TABLE(i2c, i2c_ids);
 
 struct i2c_members {
     uint8_t reg;
@@ -54,7 +46,7 @@ static struct i2c_members *i2c_data;
 
 static struct device_attribute dev_attr_register_value = {
     .attr = {
-        .name = "register-value",
+        .name = "value",
         .mode = 0666,
     },
     .show = my_show,
@@ -62,7 +54,6 @@ static struct device_attribute dev_attr_register_value = {
 };
 
 static ssize_t my_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count) {
-    printk(KERN_ALERT "store action");
     long val;
     if (kstrtol(buf, 0, &val) == 0) {
         i2c_smbus_write_byte(i2c_client, (u8)val);
@@ -71,66 +62,82 @@ static ssize_t my_store(struct device *dev, struct device_attribute *attr, const
 }
 
 static ssize_t my_show(struct device *dev, struct device_attribute *attr, char *buf) {
-    printk(KERN_ALERT "show action");
+    
     char recv[3];
     int ret = i2c_master_recv(i2c_client, recv, sizeof(recv));
-    if (ret < 0) return ret; // return error
 
     return snprintf(buf, PAGE_SIZE, "%d\n", recv[0]);
 }
-
-static int i2c_custom_probe(struct i2c_client *client, const struct i2c_device_id *id) {
-    struct device *dev = &client->dev;
+struct my_platform_data {
+    struct i2c_board_info i2c_board_info;
+};
+static int i2c_custom_probe(struct platform_device *pdev) {
+    struct device_node *np = pdev->dev.of_node;
+    struct i2c_adapter *adapter;
+    struct i2c_board_info info;
+    int reg_value, i2c_bus;
     int ret;
 
-    i2c_client = client;
+    if (!np) {
+        dev_err(&pdev->dev, "No device tree node\n");
+        return -ENODEV;
+    }
+
+    ret = of_property_read_u32(np, "reg", &reg_value);
+    ret = of_property_read_u32(np, "i2c-bus", &i2c_bus);
+    adapter = i2c_get_adapter(i2c_bus);
+    memset(&info, 0, sizeof(struct i2c_board_info));
+    strlcpy(info.type, "custom_sensor", I2C_NAME_SIZE);
+    info.addr = reg_value;
+
+    i2c_client = i2c_new_device(adapter, &info);
 
     i2c_data = kzalloc(sizeof(struct i2c_members), GFP_KERNEL);
-    if (!i2c_data) {
-        dev_err(dev, "Failed to allocate memory\n");
+    dev_set_drvdata(&pdev->dev, i2c_data);
+    i2c_custom_device = device_create(i2c_custom_class, NULL, MKDEV(0, 0), NULL, "I2C_device%d", 0);
+
+    if (device_create_file(i2c_custom_device, &dev_attr_register_value)) {
+        device_destroy(i2c_custom_class, MKDEV(0, 0));
+        kfree(i2c_data);
+        i2c_unregister_device(i2c_client);
+        i2c_put_adapter(adapter);
         return -ENOMEM;
     }
 
-    dev_set_drvdata(dev, i2c_data);
-
-    i2c_custom_device = device_create(i2c_custom_class, NULL, MKDEV(0, 0), NULL, "I2C device%d", 0);
-    if (IS_ERR(i2c_custom_device)) {
-        ret = PTR_ERR(i2c_custom_device);
-        kfree(i2c_data);
-        return ret;
-    }
-
-    ret = device_create_file(i2c_custom_device, &dev_attr_register_value);
-    if (ret) {
-        device_destroy(i2c_custom_class, MKDEV(0, 0));
-        kfree(i2c_data);
-        return ret;
-    }
-
+    i2c_put_adapter(adapter);
     return 0;
 }
 
-static int i2c_custom_remove(struct i2c_client *client) {
+static int i2c_custom_remove(struct platform_device *pdev) {
+    struct i2c_members *data = dev_get_drvdata(&pdev->dev);
     device_remove_file(i2c_custom_device, &dev_attr_register_value);
     device_destroy(i2c_custom_class, MKDEV(0, 0));
-    kfree(i2c_data);
+    kfree(data);
+    i2c_unregister_device(i2c_client);
+
     return 0;
 }
-
 static void i2c_custom_exit(void) {
-    class_destroy(i2c_custom_class);
-    //i2c_del_driver(&my_driver);
+    platform_driver_unregister(&my_driver);
+    if (i2c_custom_device) {
+        device_destroy(i2c_custom_class, MKDEV(0, 0));
+        i2c_custom_device = NULL;
+    }
+    device_remove_file(i2c_custom_device, &dev_attr_register_value);
+    if (i2c_custom_class) {
+        class_destroy(i2c_custom_class);
+        i2c_custom_class = NULL;
+    }
 }
 
 static int i2c_custom_init(void) {
     int result;
-
     i2c_custom_class = class_create(THIS_MODULE, "drvoi2c");
     if (IS_ERR(i2c_custom_class)) {
         return PTR_ERR(i2c_custom_class);
     }
 
-    result = i2c_add_driver(&my_driver);
+    result = platform_driver_register(&my_driver);
     if (result) {
         class_destroy(i2c_custom_class);
     }
